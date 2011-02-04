ce3056e4c348e8b6ff7a2edf8de58730604410f2
Alex Clark <aclark@aclark.net>
2011-02-04 10:43:24 -0500

    Prepare silly_content_generator 1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@
 from setuptools import setup
 
 setup(
+    version='1.0',
     name='silly_content_generator',
+    description='A tool to generate sample website content',
+    long_description=open('README.txt','rb').read(),
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
     py_modules=['silly_content_generator'],
     install_requires=['lorem-ipsum-generator'],
     entry_points = {
--- silly_content_generator.py
+++ silly_content_generator.py
@@ -8,15 +8,18 @@ markup_gen = MarkupGenerator()
 
 _MAGIC_NUMBER = 5
 
+
 def create_index(outfile):
         index = open(outfile, 'wb')
         index.write(markup_gen.generate_paragraphs_html_p(_MAGIC_NUMBER))
         index.close()
 
+
 def create_dirs():
     for dir in generator.generate_sentence().split(' '):
-        # http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
-        dir = dir.translate(string.maketrans("",""), string.punctuation).lower()
+        # http://stackoverflow.com/questions/265960
+        dir = dir.translate(string.maketrans("", ""),
+            string.punctuation).lower()
         print dir
         try:
             os.mkdir(dir)
@@ -24,6 +27,7 @@ def create_dirs():
             print "dir '%s' already exists" % dir
         outfile = '%s/index.html' % dir
         create_index(outfile)
+
 
 def main():
     create_index('index.html')
