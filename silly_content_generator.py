0ec2fa193264e0a2670ac382a3791b14918c8048
Alex Clark <aclark@aclark.net>
2011-02-04 08:54:05 -0500

    Make dirs

--- silly_content_generator.py
+++ silly_content_generator.py
@@ -1,4 +1,5 @@
 
+import os
 import string
 from lipsum import Generator
 
@@ -7,8 +8,9 @@ lipsum = Generator()
 def create_dirs():
     for dir in lipsum.generate_sentence().split(' '):
         # http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
-        print dir.translate(string.maketrans("",""), string.punctuation).lower()
-
+        dir = dir.translate(string.maketrans("",""), string.punctuation).lower()
+        print dir
+        os.mkdir(dir)
 
 def main():
     create_dirs()
