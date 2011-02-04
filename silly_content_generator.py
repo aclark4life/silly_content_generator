32ad863775ad42bf7703da9881c72a3dd0dbfc29
Alex Clark <aclark@aclark.net>
2011-02-04 09:16:18 -0500

    Catch dirs that exist

--- silly_content_generator.py
+++ silly_content_generator.py
@@ -10,7 +10,10 @@ def create_dirs():
         # http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
         dir = dir.translate(string.maketrans("",""), string.punctuation).lower()
         print dir
-        os.mkdir(dir)
+        try:
+            os.mkdir(dir)
+        except OSError:
+            print "dir '%s' already exists" % dir
 
 def main():
     create_dirs()
