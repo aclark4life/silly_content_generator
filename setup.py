7b9b8c0b7320e3c7b59f3850e2cd115a45010c65
Alex Clark <aclark@aclark.net>
2011-02-03 23:03:08 -0500

    Add setuptools script

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='silly_content_generator',
+    py_modules=['silly_content_generator'],
+    entry_points = {
+        'console_scripts': [
+            'silly_content_generator = silly_content_generator.silly_content_generator:main',
+        ],
+    }
+)
