7c4983b5755270707a985059831b069ff4f83577
Alex Clark <aclark@aclark.net>
2011-02-04 09:51:20 -0500

    Create index.html files

--- silly_content_generator.py
+++ silly_content_generator.py
@@ -1,12 +1,15 @@
 
 import os
 import string
-from lipsum import Generator
+from lipsum import Generator, MarkupGenerator
 
-lipsum = Generator()
+generator = Generator()
+markup_gen = MarkupGenerator()
+
+_MAGIC_NUMBER = 5
 
 def create_dirs():
-    for dir in lipsum.generate_sentence().split(' '):
+    for dir in generator.generate_sentence().split(' '):
         # http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
         dir = dir.translate(string.maketrans("",""), string.punctuation).lower()
         print dir
@@ -14,6 +17,9 @@ def create_dirs():
             os.mkdir(dir)
         except OSError:
             print "dir '%s' already exists" % dir
+        index = open('%s/index.html' % dir, 'wb')
+        index.write(markup_gen.generate_paragraphs_html_p(_MAGIC_NUMBER))
+        index.close()
 
 def main():
     create_dirs()
