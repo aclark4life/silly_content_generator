3d71ba01cd1a9a8caf67f89da99a1840ba13fd53
Alex Clark <aclark@aclark.net>
2011-02-03 23:28:36 -0500

    Get rid of punctuation

--- silly_content_generator.py
+++ silly_content_generator.py
@@ -1,11 +1,14 @@
 
+import string
 from lipsum import Generator
 
 lipsum = Generator()
 
 def create_dirs():
     for dir in lipsum.generate_sentence().split(' '):
-        print dir
+        # http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
+        print dir.translate(string.maketrans("",""), string.punctuation).lower()
+
 
 def main():
     create_dirs()
